e70b88f85cf4da16f7834ac2fe503c4b0d7fe583
Rishabhsharma2004 <rishabhsharma919319@gmail.com>
2024-06-20 21:38:12 +0530

    set Url

--- SynStream/src/App.jsx
+++ SynStream/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Project from './pages/Project'
+import Dashboard from './pages/Dashboard'
 export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl text-red-500'>App</h1>
-    </div>
+     <BrowserRouter>
+        <Routes>
+          <Route path='/' element ={<Home />}></Route>
+          <Route path='/about' element ={<About />}></Route>
+          <Route path='/sign-in' element ={<Signin />}></Route>
+          <Route path='/sign-Up' element ={<SignUp />}></Route>
+          <Route path='/project' element ={<Project />}></Route>
+          <Route path='/dashboard' element ={<Dashboard />}></Route>
+        </Routes>
+     </BrowserRouter>
   )
 }
